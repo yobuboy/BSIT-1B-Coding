cd01b981ad5f3cfd330f9dd7084d4b5a06bf84b6
Alba, Mark Andrei J. <albamarkandrei09@gmail.com>
2025-08-12 14:57:18 +0800

    Create code_challenge1.py

---
+++ code_challenge1.py
@@ -0,0 +1,2 @@
+name = input("Enter your full name: ")
+print("\t\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t*\t\t\t\t*\n\t\t*\t\tHello,\t\t\t\t*\n\t*\t\t\t ", name,"\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t\t\t*\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t\t\t*")
