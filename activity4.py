ec9d3bda461a831a8c1b404a9626b85c0123c8f6
Alba, Mark Andrei J. <albamarkandrei09@gmail.com>
2025-08-12 14:45:54 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,2 @@
+name = input("Type your name here -->")
+print("Your name has ",len(name),"characters.")
---
+++ activity5.py
@@ -0,0 +1,2 @@
+name = eval(input("Type any character here ..."))
+print("The data type of name is ",type(name))
