6744dabb08977e98d5f11c933de586eea110e7b5
Alba, Mark Andrei J. <albamarkandrei09@gmail.com>
2025-08-13 15:14:18 +0800

    Create activity6.py

---
+++ activity6.py
@@ -0,0 +1,13 @@
+number1 = eval(input("Enter the first number : "))
+number2 = eval(input("Enter the second number : "))
+plus = number1 + number2
+minus = number1 - number2
+times = number1 * number2
+divide = number1 / number2
+print("\nThe sum of",number1,"and",number2,"is",plus)
+print("The difference of",number1,"and",number2,"is",minus)
+print("The product of",number1,"and",number2,"is",times)
+print("The Quotient of",number1,"and",number2,"is",divide)
+print(number1, "exponent by", number2, "is",number1**number2)
+print("The remainder of", number1,"and",number2,"is",number1%number2)
+print("The floor division of", number1,"and",number2,"is",number1//number2)
