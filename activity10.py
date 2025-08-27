bdedb19e9e15876c219e42f6b5e4009533e7eb1c
Alba, Mark Andrei J. <albamarkandrei09@gmail.com>
2025-08-27 16:01:26 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,10 @@
+username = "akosialba"
+password = "mamamo09"
+
+uname = input("Enter username --> ")
+pword = input("Enter password --> ")
+
+if (uname == username) and (pword == password):
+    print("May Tama ka Boss")
+else:
+    print("Hayyss, Mali ka Boss")
