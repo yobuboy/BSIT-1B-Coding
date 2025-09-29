8a44e9fedc0e2b108ed8c8521018269740916ddc
Alba, Mark Andrei J. <albamarkandrei09@gmail.com>
2025-09-29 16:03:36 +0800

    pa activity ni ser

---
+++ activity20.py
@@ -0,0 +1,18 @@
+
+for drei in range(1,11,1):
+    for h in range(10, drei, -1):
+        print(" ", end=" ")
+    for g in range(1, drei, 1):
+        print("x", end=" ")
+    for i in range(1, drei,1):
+        print("x", end=" ")
+    print()
+
+for drei in range(1,11,1):
+    for h in range(1, drei, 1):
+        print(" ", end=" ")
+    for g in range(10, drei, -1):
+        print("x", end=" ")
+    for i in range(10, drei,-1):
+        print("x", end=" ")
+    print()
