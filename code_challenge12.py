01c4d0cca32c1ba0a31f356a26d8870e5057ecef
Alba, Mark Andrei J. <albamarkandrei09@gmail.com>
2025-10-06 15:22:04 +0800

    challenge ni ser!

---
+++ code_challenge12.py
@@ -0,0 +1,8 @@
+for drei in range(1,7,1):
+    for h in range(7, drei, -1):
+        print(" ", end=" ")
+    for g in range(drei,0,-1):
+        print(g, end=" ")
+    for i in range(2,drei+1,1):
+        print(i, end=" ")
+    print()
