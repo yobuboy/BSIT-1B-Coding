6b04ed5a3be540977643c36abbdf8ea509aabc97
Alba, Mark Andrei J. <albamarkandrei09@gmail.com>
2025-11-18 16:09:17 +0800

    Escape sequence
    Added comments explaining escape sequences in strings.

--- activity3.py
+++ activity3.py
@@ -1,2 +1,13 @@
-#print("Magandang Hapon \n\n\t\t\tBSIT1B")
+print("Magandang Hapon \n\n\t\t\tBSIT1B")
 print("The Quick Brown \vJumps Over the Lazy Dog.!")
+# Escape sequence gives you a way to represent special characters inside strings—characters 
+# that either can’t be typed directly, would break the string, or control formatting.
+# They make strings more powerful, flexible, and expressive.
+
+# \n - Newline
+# \t - Tab
+# \\ - Backslash
+# \' - Single quote
+# \" - Double quote
+# \r - Carriage return
+# \b - Backspace
