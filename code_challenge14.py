9ddf801dd12ac19a0cbca670621fc0c5eb5c605c
Alba, Mark Andrei J. <albamarkandrei09@gmail.com>
2025-11-19 15:51:54 +0800

    Student List Record

--- code_challenge14.py
+++ code_challenge14.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 os.system('cls')
 
@@ -37,20 +38,80 @@ while True:
         continue
     elif choice == 'b':
         os.system('cls')
+        print("PRINTING STUDENT RECORD")
         for i, j in student_record.items():
             print(f"STUDENT ID - {i}    |INFORMATION - {j}")
         continue
     elif choice == 'c':
-        pass
+        os.system('cls')
+        print("SEARCH STUDENT RECORD")
+        search_id = input("Input student id for search --> ").lower()
+        for each_id in student_record.keys():
+            if each_id in student_record.keys():
+                print("==================================")
+                print(f"\nRECORD FOUND for ID {search_id}")
+                # to print the record for the search id
+                for i in student_record[search_id]:
+                    print(f"--- {i}")
+                print("==================================")
+            else:
+                print("NO RECORD FOUND")
+            break
         continue
     elif choice == 'd':
-        pass
+        os.system('cls')
+        print("DELETE STUDENT RECORD")
+        search_id = input("Input student id for search --> ").lower()
+        for each_id in student_record.keys():
+            if each_id in student_record.keys():
+                print("==================================")
+                print(f"\nRECORD FOUND for ID {search_id}")
+                # to print the record for the search id
+                for i in student_record[search_id]:
+                    print(f"--- {i}")
+                print("==================================")
+                student_record.pop(search_id)
+                print("\nRECORD DELETE")
+            elif search_id != student_record.items():
+                print("NO RECORD FOUND")
+            else:
+                print("NO RECORD FOUND")
+            break
         continue
     elif choice == 'e':
-        pass
+        os.system('cls')
+        print("EDIT/MODIFYG STUDENT RECORD")
+        search_id = input("Input student id for search --> ").lower()
+        for each_id in student_record.keys():
+            if each_id in student_record.keys():
+                print("==================================")
+                print(f"\nRECORD FOUND for ID {search_id}")
+                # to print the record for the search id
+                for i in student_record[search_id]:
+                    print(f"--- {i}")
+                print("==================================")
+                # new set of value for searched id
+                first_name = input("Please input student First Name ---> ").upper()
+                second_name = input("Please input student Second Name ---> ").upper()
+                last_name = input("Please input student Last Name ---> ").upper()
+                age = eval(input("Input student age ---> "))
+                course = input("Input student Course ---> ").upper()
+                section = input("Input student Section ---> ").upper()
+                student_record[search_id][0] = first_name
+                student_record[search_id][1] = second_name
+                student_record[search_id][2] = last_name
+                student_record[search_id][3] = age
+                student_record[search_id][4] = course
+                student_record[search_id][5] = section
+            else:
+                print("NO RECORD FOUND")
         continue
     elif choice == 'f':
-        pass
+        os.system("cls")
+        print("EXPORT RECORD")
+        with open('student_record.json', 'w') as new_file:
+            json.dump(student_record,new_file, indent=4)
+        print("EXPORT SUCCESSFUL")
         continue
     elif choice == 'g':
         break
