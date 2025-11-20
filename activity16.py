9bcc96a1ec2cb5d159f3fc12c8f7b3290b86dcf6
Alba, Mark Andrei J. <albamarkandrei09@gmail.com>
2025-11-20 14:10:43 +0800

    Parameter

--- activity16.py
+++ activity16.py
@@ -1,4 +1,4 @@
 #the output: 1 2 3 4 5 6 7 8 9 10
 for i in range(1, 11, 1):
     print(i,end=" ")
-# this is for loop function
+# this is for loop function that modify through "print(variable,end=" ")" that make your output to the next and not a new line.
