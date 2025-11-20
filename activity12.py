4a347acdca89dc21ffeed972506f6dd8dba5adc2
Alba, Mark Andrei J. <albamarkandrei09@gmail.com>
2025-11-20 13:52:43 +0800

    For Loop

--- activity12.py
+++ activity12.py
@@ -2,8 +2,8 @@
 #for variable_x in range(start, stop, step):    =this only includes digit and optional.
 #then print
 
-#for sfp in range(1, 11, 1):  #print without number
-#    print( "Hello world")
+for sfp in range(1, 11, 1):  #print without number
+    print( "Hello world")
 
 for sfp in range(1, 11, 1): #print with number
     print(sfp, "Hello World")
