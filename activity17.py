58dfe75376e794c0f57ce10edba1dba7d3dda202
Alba, Mark Andrei J. <albamarkandrei09@gmail.com>
2025-11-20 13:42:49 +0800

    Nested For Loop

--- activity17.py
+++ activity17.py
@@ -1,8 +1,9 @@
-# for i in range(1,11,1):
-#     print(i)
+for i in range(1,11,1):
+    print(i)
 
 for i in range(1,11,1):
     # print(i,end="-")
     for x in range(1,11,1):
         print(x, end=" ")
     print()
+# this is nested for loop
