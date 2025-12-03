3749a0676366a8005069b29905a173a226997389
Alba, Mark Andrei J. <albamarkandrei09@gmail.com>
2025-12-03 15:30:23 +0800

    Add files via upload

---
+++ Python Compiler/external.py
@@ -0,0 +1,5 @@
+from internal import *
+
+def introduction():
+    while True:
+        name = input("\tA. What is Python?\n\tB. Why is Python good for beginners?\n\tC. How to use this compiler?\n\t")
---
+++ Python Compiler/internal.py
@@ -0,0 +1,14 @@
+import os
+
+
+def one():
+    os.system('cls')
+    print("Python is a high-level, versatile programming language created by Dutch software engineer Guido van")
+    print("Rossum, who began its development in the late 1980s and released the first public version in 1991. Designed")
+    print("with an emphasis on readability and simplicity, Python uses clean, English-like syntax that makes it easier for")
+    print("beginners to learn while still offering powerful capabilities for experts. Van Rossum, often referred to as")
+    print("Python\'s “Benevolent Dictator for Life” (BDFL) until he stepped down from that role in 2018, drew inspiration")
+    print("from languages like ABC and aimed to create a language that encouraged clear, logical code. Over the years,")
+    print("Python has grown into one of the world\'s most popular languages, used across web development, data")
+    print("science, automation, artificial intelligence, and scientific computing, supported by a massive global")
+    print("community and an extensive ecosystem of libraries.")
