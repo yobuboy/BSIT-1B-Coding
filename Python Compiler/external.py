65bea1388c7124f527e8d034119cff637f5eaab4
Alba, Mark Andrei J. <albamarkandrei09@gmail.com>
2025-12-07 12:16:59 +0800

    d
    Refactor introduction function to clear console and handle user input.

--- Python Compiler/external.py
+++ Python Compiler/external.py
@@ -1,5 +1,25 @@
-from internal import *
+os.system('cls')from internal import *
+import os
 
 def introduction():
+    os.system('cls')
     while True:
-        name = input("\tA. What is Python?\n\tB. Why is Python good for beginners?\n\tC. How to use this compiler?\n\t")
+        name = input("\tA. What is Python?\n\tB. Why is Python good for beginners?\n\tC. How to use this compiler?\n\tX. Exit").lower()
+        if name == 'a':
+            os.system('cls')
+            print(one)
+            continue
+        elif name == 'b':
+            os.system('cls')
+            print(two)
+            continue
+        elif name == 'c':
+            os.system('cls')
+            print(three)
+            continue
+        elif name == 'x':
+            os.system('cls')
+            break
+        else:
+            os.system('cls')
+            print("Invalid input!")
