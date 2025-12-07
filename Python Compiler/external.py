6a3795420f83b7f6575aca76a254bde95b46b209
Alba, Mark Andrei J. <albamarkandrei09@gmail.com>
2025-12-08 00:25:54 +0800

    Enhance introduction and basics functions
    Added typewriter effect and enhanced user prompts.

--- Python Compiler/external.py
+++ Python Compiler/external.py
@@ -1,22 +1,47 @@
 from internal import *
 import os
+import time
 
 def introduction():
     os.system('cls')
+    typewriter("Wellcome to Introduction of Python, Enjoy :D")
+    time.sleep(.5)
     while True:
-        name = input("\tA. What is Python?\n\tB. Why is Python good for beginners?\n\tC. How to use this compiler?\n\tX. Exit\nType here ---> ").lower()
+        name = input("\nChoose from the selection below :D\n\tA. What is Python?\n\tB. Why is Python good for beginners?\n\tC. How to use this compiler?\n\tX. Exit\nType here ---> ").lower()
         if name == 'a':
             os.system('cls')
-            one()
-            
+            one()         
         elif name == 'b':
             os.system('cls')
-            two()
-            
+            two()           
+        elif name == 'c':
+            os.system('cls')
+            three()
+        elif name == 'x':
+            os.system('cls')
+            break
+        else:
+            os.system('cls')
+            print("Invalid input!")
+
+def basics():
+    os.system('cls')
+    typewriter("Wellcome to Basics in Python, Enjoy :D")
+    time.sleep(.5)
+    while True:
+        name = input("\nChoose from the selection below :D\n\tA. Printing\n\tB. Variables\n\tC. Input\n\tD. Operators\n\tX. Exit\nType here ---> ").lower()
+        if name == 'a':
+            os.system('cls')
+            prints()         
+        elif name == 'b':
+            os.system('cls')
+            variables()           
         elif name == 'c':
             os.system('cls')
             pass
-            continue
+        elif name == 'd':
+            os.system('cls')
+            pass
         elif name == 'x':
             os.system('cls')
             break
