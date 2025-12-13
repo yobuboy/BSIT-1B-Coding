80e708fe855d23d8b9a7bae65d38706b971c30eb
Alba, Mark Andrei J. <albamarkandrei09@gmail.com>
2025-12-13 17:51:42 +0800

    Update output.py

--- Python Compiler/output.py
+++ Python Compiler/output.py
@@ -6,7 +6,8 @@ import os
 os.system('cls')
 typewriter("Hello, programmer!")
 time.sleep(.5)
-typewriter("Wellcome to the Python Learning Compiler\n")
+name = user_name()
+typewriter(f"\nWellcome {name} to the Python Learning Compiler\n")
 time.sleep(1)
 typewriter("Learn Python easily, understand each topic clearly, and start coding with confidence.\n")
 time.sleep(2)
@@ -16,7 +17,7 @@ input("\npress enter if you are ready!: ")
 os.system('cls')
 
 while True:
-    name = input("Choose from the selection below :D\n\tA. Introduction\n\tB. Basics\n\tC. Decisions\n\tD. Loops\n\tE. Collections\n\tF. Functions\n\tG. Modules\n\tH. Files\n\tI. Mini Projects\n\tJ. Quiz Section\n\tX. Exit\nType here ---> ").lower()
+    name = input("Choose from the selection below :D\n\tA. Introduction\n\tB. Basics\n\tC. Decisions\n\tD. Loops\n\tE. Collections\n\tF. Functions\n\tG. Modules\n\tH. Mini Projects\n\tI. Quiz Section\n\tX. Exit\nType here ---> ").lower()
     if name == 'a':
         introduction()
     elif name == 'b':
@@ -32,10 +33,8 @@ while True:
     elif name == 'g':
         moduless()
     elif name == 'h':
-        pass
+        mini_proj()
     elif name == 'i':
-        pass
-    elif name == 'j':
         pass
     elif name == 'x':
         break
